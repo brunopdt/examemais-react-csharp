a52007b4e5ef41ff9426238c0ddcb028424f5f7b
bruno duarte <bruno.pduarte0@gmail.com>
2024-01-05 21:51:48 -0300

    changed prop to readonly

--- views/src/modules/Login/components/card.tsx
+++ views/src/modules/Login/components/card.tsx
@@ -4,7 +4,7 @@ interface ICardProps {
   children: ReactNode
 }
 
-export function Card({ children }: ICardProps) {
+export function Card({ children }: Readonly<ICardProps>) {
   return (
     <div className="flex items-center h-svh">
       <div className="h-fit w-1/3 bg-background rounded-xl ml-20">
