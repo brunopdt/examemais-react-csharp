c7ab21fd418b853bda14a9748986b39b41b973d8
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2024-01-14 13:23:37 -0300

    #29

--- api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e4fc192c41996ce522acb833d9977f719e17b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
